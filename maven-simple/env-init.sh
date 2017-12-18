0edfcbf77de8cb6d834a7fa90e7f63045f396f3f
Matthieu Brouillard <matthieu@brouillard.fr>
2017-12-18 21:25:26 +0100

    add git, create initial maven project

---
+++ maven-simple/env-init.sh
@@ -0,0 +1,16 @@
+sudo apt-get update && sudo apt-get install git -y
+mvn archetype:generate \
+  -DgroupId=fr.brouillard.jgitver.demos \
+  -DartifactId=simple-maven-demo \
+  -Dversion=0 \
+  -Dpackage=fr.brouillard.jgitver.demos \
+  -DarchetypeArtifactId=maven-archetype-quickstart \
+  -DinteractiveMode=false \
+  -DarchetypeCatalog=local
+cd simple-maven-demo
+git init
+git config user.name "Scrap Kata-Coda"
+git config user.email "scrapbook@nowhere.com"
+echo target/ > .gitignore
+git add .
+git commit -m "initial version"
