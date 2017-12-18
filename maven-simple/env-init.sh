6f8e5a0ae360fa8e3e6cf3b1da9f5dc27d998ee8
Matthieu Brouillard <matthieu@brouillard.fr>
2017-12-18 21:56:59 +0100

    configure all in background

--- maven-simple/env-init.sh
+++ maven-simple/env-init.sh
@@ -1,2 +1,19 @@
 sudo apt-get update && sudo apt-get install git -y
 sudo git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+mvn archetype:generate \
+  -DgroupId=fr.brouillard.jgitver.demos \
+  -DartifactId=simple-maven-demo \
+  -Dversion=0 \
+  -Dpackage=fr.brouillard.jgitver.demos \
+  -DarchetypeArtifactId=maven-archetype-quickstart \
+  -DinteractiveMode=false \
+  -DarchetypeCatalog=local
+cd simple-maven-demo
+
+git init
+git config user.name "Scrap Kata-Coda"
+git config user.email "scrapbook@nowhere.com"
+echo target/ > .gitignore
+git add .
+git commit -m "initial version"
