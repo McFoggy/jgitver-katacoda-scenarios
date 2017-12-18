780269d2320ee2fd27b50c43c84937181d326b0f
Matthieu Brouillard <matthieu@brouillard.fr>
2017-12-18 22:55:36 +0100

    split git and python installation

--- maven-simple/env-init.sh
+++ maven-simple/env-init.sh
@@ -1,2 +1,3 @@
-sudo apt-get update && sudo apt-get install git python -y
+sudo apt-get update && sudo apt-get install git -y
 sudo git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+sudo apt-get install python -y
