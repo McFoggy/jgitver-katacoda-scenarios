e3e06aab4f3e4e2baf14f705859c1e5c616b6bbf
Matthieu Brouillard <matthieu@brouillard.fr>
2017-12-19 08:57:48 +0100

    do init in a single step

--- maven-simple/tools-init.sh
+++ maven-simple/tools-init.sh
@@ -1,3 +1,3 @@
-sudo apt-get update && sudo apt-get install git jq -y
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+sudo apt-get update && sudo apt-get install git jq -y && \
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" && \
 clear
